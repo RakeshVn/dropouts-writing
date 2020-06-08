97c2070756e7afdd145ed8ee554ad43973365709
unknown <rakesh.vangari@foyr.com>
2020-06-08 16:23:16 +0530

    Services fixed

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonService } from '../services/common.service';
 declare const $: any
 @Component({
@@ -8,11 +8,18 @@ declare const $: any
 })
 export class HomepageComponent implements OnInit {
 
-  counto = 0
+  @ViewChild('service1', { static: false }) service1
+  @ViewChild('service2', { static: false }) service2
+  @ViewChild('service3', { static: false }) service3
   counters = {
     a: 5,
     b: 15000,
     c: 4800000
+  }
+  services = {
+    a: false,
+    b: false,
+    c: false
   }
 
   constructor(
@@ -23,16 +30,31 @@ export class HomepageComponent implements OnInit {
   }
 
   onSubmit(type, formData) {
-    console.log(type, formData)
     this._CommonService.post(`event/${type}`, formData).subscribe(res => {
       console.log(res)
     })
   }
 
-  onService(service: HTMLElement) {
+  onService(type, service: HTMLElement) {
+
+    for (const iterator in this.services) {
+      if (iterator == type) {
+        this.services[type] = !this.services[type]
+        continue
+      }
+      this.services[iterator] = false
+    }
+
     setTimeout(() => {
-      service.scrollIntoView({ behavior: "smooth" });
+      if (type == 'a') {
+        this.service1.nativeElement.scrollIntoView({ behavior: "smooth" });
+      } else if (type == 'b') {
+        this.service2.nativeElement.scrollIntoView({ behavior: "smooth" });
+      } else {
+        this.service3.nativeElement.scrollIntoView({ behavior: "smooth" });
+      }
     }, 200);
+
   }
 
 }
